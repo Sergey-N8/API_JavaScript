a84e06d8a467f470d4895afb4ac404a31e808479
Sergey-N8 <keerfeek@gmail.com>
2024-05-16 01:07:46 +0300

    hw_1

---
+++ hw_1/data.js
@@ -0,0 +1,24 @@
+export const workouts = `
+[
+    {
+        "id": "1",
+        "name_of_workout": "Лепка из глины",
+        "time_of_workout": "10:00 - 12:00",
+        "max_number_of_participants": "10",
+        "current_number_of_participants": "6"
+    },
+    {
+        "id": "2",
+        "name_of_workout": "Гитара",
+        "time_of_workout": "13:00 - 15:00",
+        "max_number_of_participants": "15",
+        "current_number_of_participants": "2"
+    },
+    {
+        "id": "3",
+        "name_of_workout": "Кросфит",
+        "time_of_workout": "16:00 - 18:00",
+        "max_number_of_participants": "15",
+        "current_number_of_participants": "10"
+    }
+]`
